a54f68c33123cfbcf385f46ab8a7182432611db3
DevTom <72770851+Olatomiw@users.noreply.github.com>
2024-09-12 23:25:18 +0100

    Security Config

---
+++ src/main/java/org/example/recipe_sharing_app/config/JWTConfig.java
@@ -0,0 +1,71 @@
+package org.example.recipe_sharing_app.config;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JWTConfig {
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+    @Value("${jwt.secret.expiry}")
+    private int expiry;
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+        Date createdDate = new Date();
+        Date expiryDate = new Date(createdDate.getTime() + expiry);
+        String token = Jwts
+                .builder()
+                .setSubject(username)
+                .setIssuedAt(createdDate)
+                .setExpiration(expiryDate)
+                .signWith(getSecretKey(), SignatureAlgorithm.HS256)
+                .compact();
+        return token;
+    }
+
+    public String extractUsername(String token) {
+        return Jwts
+                .parser()
+                .verifyWith((SecretKey) getSecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public Date extractExpiration(String token) {
+        return Jwts
+                .parser()
+                .verifyWith((SecretKey) getSecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration();
+    }
+
+    public Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Boolean validateToken(String token, Authentication authentication) {
+        String username = authentication.getPrincipal().toString();
+        return extractUsername(token).equals(username) && !isTokenExpired(token);
+    }
+
+    public Key getSecretKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+}
--- src/main/java/org/example/recipe_sharing_app/config/SecurityConfig.java
+++ src/main/java/org/example/recipe_sharing_app/config/SecurityConfig.java
@@ -3,10 +3,17 @@ package org.example.recipe_sharing_app.config;
 import jakarta.persistence.Column;
 import jakarta.servlet.Filter;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,16 +21,33 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig  {
 
+    private final AuthEntryPoint authEntryPoint;
+    @Bean
     public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
         DefaultSecurityFilterChain securityFilterChain = http
                 .csrf(e->e.disable())
                 .authorizeHttpRequests(e->e
                         .anyRequest().authenticated())
+                .exceptionHandling(e->e
+                        .authenticationEntryPoint(authEntryPoint))
+                .sessionManagement(e->e
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .formLogin(Customizer.withDefaults())
                 .build();
 
         return securityFilterChain;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder (){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
     }
 }
